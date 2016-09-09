eb0e4db19c23b5c25e2d45b8708ddaba6176eea3
Agustin <agustin.pane@gmail.com>
2016-09-09 13:36:03 -0300

    ListView funcionando

--- app/src/main/java/com/example/agustin/lab03c2016/AdapterOfertas.java
+++ app/src/main/java/com/example/agustin/lab03c2016/AdapterOfertas.java
@@ -4,14 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,10 +20,13 @@ import java.util.List;
  */
 public class AdapterOfertas extends BaseAdapter {
     private LayoutInflater inflater;
-    private TextView nombreEmpresa,nombreTrabajo,cantHoras,ingresoPorHora,fecha;
+    private TextView nombreProyecto,nombreTrabajo,cantHoras,ingresoPorHora,fecha;
     private ImageView banderaPais;
     private View row;
     private List<Trabajo> items;
+    private CheckBox cbRequiereIngles;
+    private DecimalFormat decimalFormat;
+    private SimpleDateFormat dateFormat;
     public AdapterOfertas(Context context, List<Trabajo> items)
     {
        //super(context, R.layout.row_trabajos,items);
@@ -53,7 +57,7 @@ public class AdapterOfertas extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        DecimalFormat df = new DecimalFormat("#.##");
+
 
         row = convertView;
         if(row == null)
@@ -61,33 +65,45 @@ public class AdapterOfertas extends BaseAdapter {
             row = inflater.inflate(R.layout.row_trabajos,parent,false);
         }
         cargarVariables();
-        //llenarAdapter(position);
-        ((TextView)row.findViewById(R.id.tvRolTrabajo)).setText( "DIGO ALGOOOO");
+        llenarAdapter(position);
+       // ((TextView)row.findViewById(R.id.tvRolTrabajo)).setText( "DIGO ALGOOOO");
 
         /**
          * ACA HACER LOGICA PARA BANDERITA Y ETC
          */
         return(row);
     }
+
+    /**
+     * Inicializa la fila de listview correspondiente con los componentes
+     */
     private void cargarVariables()
     {
-        nombreEmpresa = (TextView) row.findViewById(R.id.tvNombreEmpresa);
+        nombreProyecto = (TextView) row.findViewById(R.id.tvNombreEmpresa);
         nombreTrabajo = (TextView) row.findViewById(R.id.tvRolTrabajo);
         cantHoras = (TextView) row.findViewById(R.id.tvCantHoras);
         ingresoPorHora = (TextView) row.findViewById(R.id.tvIngresoPorHora_plata);
         fecha = (TextView) row.findViewById(R.id.tvFecha);
         banderaPais = (ImageView) row.findViewById(R.id.imageViewBanderaPais);
+        cbRequiereIngles = (CheckBox) row.findViewById(R.id.checkBoxEnIngles);
     }
-    private void llenarAdapter(int position)
-    {
-       // nombreEmpresa.setText( (Trabajo) this.getItem(position).getD() );
-        nombreTrabajo.setText ( ((Trabajo) this.getItem(position)).getCategoria().getDescripcion());
-        /*
-        cantHoras = (TextView) this.getItem(position);
-        ingresoPorHora = (TextView) this.getItem(position);
-        fecha = (TextView) this.getItem(position);
-        banderaPais = (ImageView) this.getItem(position);
-        */
+
+    /**
+     * Carga los componentes de la fila position del listview
+     * @param position
+     */
+    private void llenarAdapter(int position) {
+        decimalFormat = new DecimalFormat("#.##");
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Double precioMaximoHora = ( ((Trabajo) this.getItem(position)).getPrecioMaximoHora());
+        Date fechaEntrega = ((Trabajo) this.getItem(position)).getFechaEntrega();
+
+        nombreProyecto.setText(((Trabajo) this.getItem(position)).getDescripcion());
+        nombreTrabajo.setText(((Trabajo) this.getItem(position)).getCategoria().getDescripcion());
+        cantHoras.setText(" "+((Trabajo) this.getItem(position)).getHorasPresupuestadas().toString()+" ");
+        ingresoPorHora.setText(" "+decimalFormat.format(precioMaximoHora));
+        fecha.setText(" "+dateFormat.format(fechaEntrega));
+        cbRequiereIngles.setChecked(((Trabajo) this.getItem(position)).getRequiereIngles() );
     }
 
 }
